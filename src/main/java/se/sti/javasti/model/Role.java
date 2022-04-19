233a08b31d9ae14228d48b36953a263bc4633676
aleexf <alexander.frodin@gmail.com>
2022-04-19 10:39:03 +0200

    generated value + getters and setters

--- src/main/java/se/sti/javasti/model/Role.java
+++ src/main/java/se/sti/javasti/model/Role.java
@@ -1,17 +1,31 @@
 package se.sti.javasti.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "roles")
 public class Role {
     @Id
     @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "role_name")
     private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
