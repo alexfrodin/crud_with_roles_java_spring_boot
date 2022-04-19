4cc13df7d1594c67fdb1113be8efc0d9255b9588
aleexf <alexander.frodin@gmail.com>
2022-04-19 14:32:16 +0200

    req mapping for save student

--- src/main/java/se/sti/javasti/controller/StudentController.java
+++ src/main/java/se/sti/javasti/controller/StudentController.java
@@ -3,6 +3,7 @@ package se.sti.javasti.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import se.sti.javasti.model.Student;
 import se.sti.javasti.services.StudentService;
@@ -31,5 +32,11 @@ public class StudentController {
         Student student = new Student();
         model.addAttribute(student);
         return "new_student";
+    }
+
+    @RequestMapping("/save")
+    public String createStudent(@ModelAttribute("student") Student student) {
+        studentService.createStudent(student);
+        return "redirect:/";
     }
 }
