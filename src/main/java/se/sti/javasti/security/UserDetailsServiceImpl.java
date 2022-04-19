125e06dee765b0266fa1081ce19b1b5f52fde0a1
aleexf <alexander.frodin@gmail.com>
2022-04-19 11:05:19 +0200

    user details service

--- src/main/java/se/sti/javasti/security/UserDetailsServiceImpl.java
+++ src/main/java/se/sti/javasti/security/UserDetailsServiceImpl.java
@@ -1,2 +1,25 @@
-package se.sti.javasti.security;public class UserDetailsServiceImpl {
+package se.sti.javasti.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import se.sti.javasti.model.User;
+import se.sti.javasti.repository.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("Username %s not found", username)));
+        return new CustomUserDetails(user);
+    }
 }
