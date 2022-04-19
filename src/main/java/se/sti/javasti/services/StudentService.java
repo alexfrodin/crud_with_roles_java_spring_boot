956d8306b442821798eb4a6455f6188b212e8976
aleexf <alexander.frodin@gmail.com>
2022-04-19 13:34:33 +0200

    student service interface + impl

--- src/main/java/se/sti/javasti/services/StudentService.java
+++ src/main/java/se/sti/javasti/services/StudentService.java
@@ -1,2 +1,15 @@
-package se.sti.javasti.services;public interface StudentService {
+package se.sti.javasti.services;
+
+import se.sti.javasti.model.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> listAllStudents();
+
+    void createStudent(Student student);
+
+    Student getStudent(Long id);
+
+    void deleteStudent(Long id);
 }
--- src/main/java/se/sti/javasti/services/StudentServiceImpl.java
+++ src/main/java/se/sti/javasti/services/StudentServiceImpl.java
@@ -1,2 +1,40 @@
-package se.sti.javasti.services;public class StudentServiceImpl {
+package se.sti.javasti.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import se.sti.javasti.model.Student;
+import se.sti.javasti.repository.StudentRepository;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> listAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public void createStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudent(Long id) {
+        return studentRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("No student found"));
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
 }
