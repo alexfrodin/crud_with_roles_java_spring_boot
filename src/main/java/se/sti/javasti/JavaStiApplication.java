a6fbc981cde5d688b63e221a3744f1fb02c9791d
aleexf <alexander.frodin@gmail.com>
2022-04-19 10:34:59 +0200

    init

---
+++ src/main/java/se/sti/javasti/JavaStiApplication.java
@@ -0,0 +1,13 @@
+package se.sti.javasti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/sti/javasti/JavaStiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.sti.javasti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
