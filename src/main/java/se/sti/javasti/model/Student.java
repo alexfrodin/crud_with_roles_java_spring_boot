5c91136117a24dde3a7e761b324dfb34d2869028
aleexf <alexander.frodin@gmail.com>
2022-04-19 12:10:43 +0200

    student model

--- src/main/java/se/sti/javasti/model/Student.java
+++ src/main/java/se/sti/javasti/model/Student.java
@@ -1,2 +1,42 @@
-package se.sti.javasti.model;public class Student {
+package se.sti.javasti.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private Long studentId;
+
+    @Column(name = "student_name")
+    private String studentName;
+
+    @Column(name = "student_age")
+    private Integer studentAge;
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public Integer getStudentAge() {
+        return studentAge;
+    }
+
+    public void setStudentAge(Integer studentAge) {
+        this.studentAge = studentAge;
+    }
 }
