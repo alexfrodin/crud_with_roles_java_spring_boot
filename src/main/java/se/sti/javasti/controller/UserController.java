7895a8091a4edcbc998f978455bcb50d59cadbdf
aleexf <alexander.frodin@gmail.com>
2022-04-20 14:05:59 +0200

    controller to handle API users

--- src/main/java/se/sti/javasti/controller/UserController.java
+++ src/main/java/se/sti/javasti/controller/UserController.java
@@ -1,2 +1,35 @@
-package se.sti.javasti.controller;public class UserController {
+package se.sti.javasti.controller;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.sti.javasti.controller.response.OkResponseBody;
+import se.sti.javasti.dto.UserResponseDTO;
+import se.sti.javasti.repository.UserRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final ModelMapper mapper;
+
+    @Autowired
+    public UserController(UserRepository userRepository, ModelMapper mapper) {
+        this.userRepository = userRepository;
+        this.mapper = mapper;
+    }
+
+    @GetMapping()
+    public OkResponseBody<List<UserResponseDTO>> getAllUsers() {
+        List<UserResponseDTO> userResponseDTOList = userRepository.findAll().stream()
+                .map(user -> mapper.map(user, UserResponseDTO.class)).collect(Collectors.toList());
+        return new OkResponseBody<>("All users", userResponseDTOList);
+    }
+
 }
