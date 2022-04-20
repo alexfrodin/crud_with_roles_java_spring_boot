7c3529345b152e23ca5869f7aca1b45f3be7358e
aleexf <alexander.frodin@gmail.com>
2022-04-20 14:04:21 +0200

    adding modelmapper bean

--- src/main/java/se/sti/javasti/config/ApplicationConfig.java
+++ src/main/java/se/sti/javasti/config/ApplicationConfig.java
@@ -1,2 +1,15 @@
-package se.sti.javasti.config;public class ApplicationConfig {
+package se.sti.javasti.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
 }
