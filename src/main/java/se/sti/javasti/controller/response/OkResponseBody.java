0a3a3ac66988abf18b3fff4ced8d338407b22b89
aleexf <alexander.frodin@gmail.com>
2022-04-20 14:04:42 +0200

    reponse body for API requests

--- src/main/java/se/sti/javasti/controller/response/OkResponseBody.java
+++ src/main/java/se/sti/javasti/controller/response/OkResponseBody.java
@@ -1,4 +1,28 @@
 package se.sti.javasti.controller.response;
 
-public class OkResponse {
+import java.time.LocalDateTime;
+
+public class OkResponseBody<E> {
+
+    private String date;
+    private String message;
+    private E data;
+
+    public OkResponseBody(String message, E data) {
+        this.date = LocalDateTime.now().toString();
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public E getData() {
+        return data;
+    }
 }
