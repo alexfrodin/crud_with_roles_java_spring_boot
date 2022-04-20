4fef1178975e47d1e972339287bb1d45739c8812
aleexf <alexander.frodin@gmail.com>
2022-04-20 12:48:50 +0200

    add one more role to test multiple roles on user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,3 +11,4 @@ INSERT INTO roles (role_id, role_name) VALUES (4, 'ADMIN');
 INSERT INTO users (user_name, user_password) VALUES ('Alexander', '$2a$10$OlpVGGz1EXm.LQ/OcvmBQOFdAe3FQNYhOOXrKD6y9fhxOr2aBKwHu');
 
 INSERT INTO users_roles (user_id, role_id) VALUES (1, 4);
+INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
