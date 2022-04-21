6a2b58ee9fadc0beb1e67911189558523f0235b1
aleexf <alexander.frodin@gmail.com>
2022-04-21 09:44:56 +0200

    controller advisor for http exceptions

---
+++ src/main/java/se/sti/javasti/controller/ControllerAdvisor.java
@@ -0,0 +1,26 @@
+package se.sti.javasti.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import se.sti.javasti.exeption.UserAlreadyExistsException;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ControllerAdvisor {
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<Object> handleUserAlreadyExistsException(UserAlreadyExistsException e) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", String.format("%s", HttpStatus.BAD_REQUEST));
+        body.put("message", e.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+}
---
+++ src/main/java/se/sti/javasti/exeption/UserAlreadyExistsException.java
@@ -0,0 +1,2 @@
+package se.sti.javasti.exeption;public class UserAlreadyExistsException {
+}
