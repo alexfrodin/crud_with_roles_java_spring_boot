ee29af554e030b581c578fa8ff7e15f35accdd85
aleexf <alexander.frodin@gmail.com>
2022-04-21 09:47:58 +0200

    enable prePost for authority check in controllers and remove check in security config for API route

--- src/main/java/se/sti/javasti/security/BasicSecurityConfig.java
+++ src/main/java/se/sti/javasti/security/BasicSecurityConfig.java
@@ -3,18 +3,24 @@ package se.sti.javasti.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http
+                .antMatcher("/api/**")
                 .authorizeRequests()
-                .antMatchers("/api/**").hasAnyAuthority("ADMIN")
+                .anyRequest()
+                .authenticated()
                 .and()
                 .httpBasic();
     }
