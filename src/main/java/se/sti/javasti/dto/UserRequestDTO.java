c39d589886fa0131740551613b2a065417e4b306
aleexf <alexander.frodin@gmail.com>
2022-04-21 09:50:56 +0200

    DTO for user requests

--- src/main/java/se/sti/javasti/dto/UserRequestDTO.java
+++ src/main/java/se/sti/javasti/dto/UserRequestDTO.java
@@ -1,2 +1,47 @@
-package se.sti.javasti.dto;public class UserRequestDTO {
+package se.sti.javasti.dto;
+
+import org.springframework.validation.annotation.Validated;
+import se.sti.javasti.model.Role;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+@Validated
+public class UserRequestDTO {
+
+    @NotNull(message = "Username may not be null")
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotNull(message = "Password may not be null")
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotNull(message = "At least one role is required")
+    private Set<Role> roles;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
