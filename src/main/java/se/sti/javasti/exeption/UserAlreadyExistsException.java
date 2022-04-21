3f44484f54e719582a769d5a588a564a3635fa86
aleexf <alexander.frodin@gmail.com>
2022-04-21 09:45:15 +0200

    user already exists exception

--- src/main/java/se/sti/javasti/exeption/UserAlreadyExistsException.java
+++ src/main/java/se/sti/javasti/exeption/UserAlreadyExistsException.java
@@ -1,2 +1,8 @@
-package se.sti.javasti.exeption;public class UserAlreadyExistsException {
+package se.sti.javasti.exeption;
+
+public class UserAlreadyExistsException extends RuntimeException {
+
+    public UserAlreadyExistsException(String username) {
+        super(String.format("Username %s already exists", username));
+    }
 }
