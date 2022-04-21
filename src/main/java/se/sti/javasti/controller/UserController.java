27137e53e7a4d428d35c46310dc1fb17fe8442ba
aleexf <alexander.frodin@gmail.com>
2022-04-21 09:49:58 +0200

    adding delete + post mappings + preAuthorize

--- src/main/java/se/sti/javasti/controller/UserController.java
+++ src/main/java/se/sti/javasti/controller/UserController.java
@@ -2,34 +2,61 @@ package se.sti.javasti.controller;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import se.sti.javasti.controller.response.OkResponseBody;
+import se.sti.javasti.dto.UserRequestDTO;
 import se.sti.javasti.dto.UserResponseDTO;
-import se.sti.javasti.repository.UserRepository;
+import se.sti.javasti.model.Role;
+import se.sti.javasti.model.User;
+import se.sti.javasti.services.UserService;
 
+import javax.validation.Valid;
+import java.security.Principal;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final ModelMapper mapper;
 
     @Autowired
-    public UserController(UserRepository userRepository, ModelMapper mapper) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService, ModelMapper mapper) {
+        this.userService = userService;
         this.mapper = mapper;
     }
 
+    @GetMapping("/my-roles")
+    public OkResponseBody<Set<Role>> getMyRoles(Principal principal) {
+        return new OkResponseBody<>("Your roles", userService.getMyRoles(principal.getName()));
+    }
+
     @GetMapping()
+    @PreAuthorize("hasAuthority('ADMIN')")
     public OkResponseBody<List<UserResponseDTO>> getAllUsers() {
-        List<UserResponseDTO> userResponseDTOList = userRepository.findAll().stream()
-                .map(user -> mapper.map(user, UserResponseDTO.class)).collect(Collectors.toList());
+        List<UserResponseDTO> userResponseDTOList = userService.getAllUsers().stream()
+                .map(user -> mapper.map(user, UserResponseDTO.class))
+                .collect(Collectors.toList());
         return new OkResponseBody<>("All users", userResponseDTOList);
+    }
+
+    @DeleteMapping("/{userId}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void deleteUser(@PathVariable(name = "userId") String userId) {
+        System.out.println(userId);
+    }
+
+    @PostMapping()
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public OkResponseBody<UserResponseDTO> updateUser(@RequestBody @Valid UserRequestDTO userRequestDTO) {
+        User userToCreate = mapper.map(userRequestDTO, User.class);
+        return new OkResponseBody<>("Created user", mapper.map(
+                userService.createUser(userToCreate), UserResponseDTO.class
+        ));
     }
 
 }
