8fd34cda1a0378b0161655e69d2f865dc6351441
Alexander Frödin <alexanderdelaval@gmail.com>
2022-04-22 14:38:58 +0200

    added bean to generate hashed password for manual insert in DB

--- src/main/java/se/sti/javasti/JavaStiApplication.java
+++ src/main/java/se/sti/javasti/JavaStiApplication.java
@@ -2,6 +2,8 @@ package se.sti.javasti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class JavaStiApplication {
@@ -9,5 +11,12 @@ public class JavaStiApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaStiApplication.class, args);
     }
+
+    //@Bean
+    //public void runThisAtStartUpToGetHashedPassword() {
+    //    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    //    String password = passwordEncoder.encode("test");
+    //    System.out.println(password);
+    //}
 
 }
