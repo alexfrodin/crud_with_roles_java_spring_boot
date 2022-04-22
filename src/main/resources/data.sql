b1f3d6a33f0c9095776629e6733cbf1689127206
Alexander Frödin <alexanderdelaval@gmail.com>
2022-04-22 14:39:19 +0200

    added users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,15 @@ INSERT INTO roles (role_id, role_name) VALUES (2, 'CREATOR');
 INSERT INTO roles (role_id, role_name) VALUES (3, 'EDITOR');
 INSERT INTO roles (role_id, role_name) VALUES (4, 'ADMIN');
 
-INSERT INTO users (user_name, user_password) VALUES ('Alexander', '$2a$10$OlpVGGz1EXm.LQ/OcvmBQOFdAe3FQNYhOOXrKD6y9fhxOr2aBKwHu');
+INSERT INTO users (user_name, user_password) VALUES ('User', '$2a$10$NlcuuHfAF2trDvWpkmdoEO6.Ookdtixqg25kQLmMCC4od5zf9fpmS');
+INSERT INTO users (user_name, user_password) VALUES ('Creator', '$2a$10$NlcuuHfAF2trDvWpkmdoEO6.Ookdtixqg25kQLmMCC4od5zf9fpmS');
+INSERT INTO users (user_name, user_password) VALUES ('Editor', '$2a$10$NlcuuHfAF2trDvWpkmdoEO6.Ookdtixqg25kQLmMCC4od5zf9fpmS');
+INSERT INTO users (user_name, user_password) VALUES ('Admin', '$2a$10$NlcuuHfAF2trDvWpkmdoEO6.Ookdtixqg25kQLmMCC4od5zf9fpmS');
+INSERT INTO users (user_name, user_password) VALUES ('Multi', '$2a$10$NlcuuHfAF2trDvWpkmdoEO6.Ookdtixqg25kQLmMCC4od5zf9fpmS');
 
-INSERT INTO users_roles (user_id, role_id) VALUES (1, 4);
 INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO users_roles (user_id, role_id) VALUES (2, 2);
+INSERT INTO users_roles (user_id, role_id) VALUES (3, 3);
+INSERT INTO users_roles (user_id, role_id) VALUES (4, 4);
+INSERT INTO users_roles (user_id, role_id) VALUES (5, 1);
+INSERT INTO users_roles (user_id, role_id) VALUES (5, 3);
