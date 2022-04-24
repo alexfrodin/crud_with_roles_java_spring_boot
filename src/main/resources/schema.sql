4d3c8cffd922ad1033c68e14cbb4bf83740d53c5
Alexander Frödin <alexanderdelaval@gmail.com>
2022-04-24 21:55:56 +0200

    added student

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -21,6 +21,5 @@ CREATE TABLE users (
 CREATE TABLE users_roles (
     user_id BIGINT NOT NULL,
     role_id INTEGER NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES(user_id),
-    FOREIGN KEY(role_id) REFERENCES(role_id)
+    FOREIGN KEY(user_id) REFERENCES(user_id)
 )
