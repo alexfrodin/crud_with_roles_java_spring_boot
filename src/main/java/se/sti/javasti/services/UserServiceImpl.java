006550e53cd94ee775b295d2961a310abd3fefeb
Alexander Frödin <alexanderdelaval@gmail.com>
2022-04-25 09:39:10 +0200

    update user service DONE

--- src/main/java/se/sti/javasti/services/UserServiceImpl.java
+++ src/main/java/se/sti/javasti/services/UserServiceImpl.java
@@ -4,25 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import se.sti.javasti.exeption.RoleNotExistsException;
 import se.sti.javasti.exeption.UserAlreadyExistsException;
 import se.sti.javasti.model.Role;
 import se.sti.javasti.model.User;
+import se.sti.javasti.repository.RoleRepository;
 import se.sti.javasti.repository.UserRepository;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -41,7 +47,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User createUser(User user) {
         Optional<User> userOptional = userRepository.getUserByUsername(user.getUsername());
-        if(userOptional.isPresent()) throw new UserAlreadyExistsException(userOptional.get().getUsername());
+        if (userOptional.isPresent()) throw new UserAlreadyExistsException(userOptional.get().getUsername());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
@@ -52,17 +58,28 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public User updateUser(Long id, User updatedUser) {
-        User userToUpdate = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("User not found"));
-        updatedUser.setId(userToUpdate.getId());
-        if(updatedUser.getUsername() == null) updatedUser.setUsername(userToUpdate.getUsername());
-        if (updatedUser.getPassword() == null) {
-            updatedUser.setPassword(userToUpdate.getPassword());
-        } else {
-            updatedUser.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
-        }
-        if(updatedUser.getIsEnabled() == null) updatedUser.setIsEnabled(userToUpdate.getIsEnabled());
-        if(updatedUser.getRoles() == null) updatedUser.setRoles(userToUpdate.getRoles());
-        return userRepository.save(updatedUser);
+        User userToUpdate = userRepository.findById(id)
+                .map((user) -> {
+                            if (updatedUser.getUsername() != null)
+                                user.setUsername(updatedUser.getUsername());
+                            if (updatedUser.getPassword() != null)
+                                user.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+                            if (updatedUser.getIsEnabled() != null)
+                                user.setIsEnabled(updatedUser.getIsEnabled());
+                            if (!updatedUser.getRoles().isEmpty()) {
+                                Set<Role> newRoles = updatedUser.getRoles().stream().map(role -> {
+                                    Role foundRole = roleRepository.findByName(role.getName())
+                                            .orElseThrow(() -> new RoleNotExistsException(role.getName()));
+                                    return foundRole;
+                                }).collect(Collectors.toSet());
+                                user.setRoles(newRoles);
+                            }
+                            return user;
+                        }
+                )
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+        return userRepository.save(userToUpdate);
     }
 }
