696ab6f1e8dc8547c598609e9a0acb8fed25b80b
Alexander Frödin <alexanderdelaval@gmail.com>
2022-04-25 09:40:35 +0200

    repo for roles

--- src/main/java/se/sti/javasti/repository/RoleRepository.java
+++ src/main/java/se/sti/javasti/repository/RoleRepository.java
@@ -1,2 +1,12 @@
-package se.sti.javasti.repository;public interface RoleRepository {
+package se.sti.javasti.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.sti.javasti.model.Role;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String name);
+
 }
