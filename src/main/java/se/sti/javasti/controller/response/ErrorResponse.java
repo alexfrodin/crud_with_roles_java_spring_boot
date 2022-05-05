6e5c9e591cd97c4c8bf8c561757aa6c3c314b8ff
Alexander Frödin <alexanderdelaval@gmail.com>
2022-05-05 08:37:06 +0200

    pojo for error response

--- src/main/java/se/sti/javasti/controller/response/ErrorResponse.java
+++ src/main/java/se/sti/javasti/controller/response/ErrorResponse.java
@@ -1,2 +1,22 @@
-package se.sti.javasti.controller.response;public class ErrorResponse {
+package se.sti.javasti.controller.response;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ErrorResponse {
+
+    private Map<String, Object> error = new LinkedHashMap<>();
+
+    public ErrorResponse(String message, HttpStatus status) {
+        error.put("Timestamp", LocalDateTime.now());
+        error.put("Message", message);
+        error.put("Status", status);
+    }
+
+    public Map<String, Object> getErrorResponse() {
+        return error;
+    }
 }
